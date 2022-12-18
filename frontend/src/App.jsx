8083ef8e193830f3206bb4915556b33dc9ceeb95
Tim La <tlamarketing111@gmail.com>
2022-12-18 06:55:15 -0500

    recreated react app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-
+import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(1)
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div className="App">
       {count}
     </div>
   )
