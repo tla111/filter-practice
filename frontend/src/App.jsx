a81ab4254d38f96c724717c523db805fad69c728
Tim La <tlamarketing111@gmail.com>
2022-12-28 16:06:48 -0500

    removed todo app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import FilterNames from './components/filterNames'
 import DropdownMenu from './components/DropdownMenu'
-import TodoList from './components/Todo'
 
 function App() {
 
   return (
     <div className="App">
       {/* <FilterNames /> */}
-      {/* <DropdownMenu /> */}
-      <TodoList />
+      <DropdownMenu />
     </div>
   )
 }
