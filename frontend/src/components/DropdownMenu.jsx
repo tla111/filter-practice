5955d55a7ca31112645b7d186f55b72d557cd3c9
Tim La <tlamarketing111@gmail.com>
2022-12-28 14:12:55 -0500

    only displays selected option when show is true

--- frontend/src/components/DropdownMenu.jsx
+++ frontend/src/components/DropdownMenu.jsx
@@ -3,6 +3,7 @@ import Select from 'react-select'
 
 const DropdownMenu = () => {
     const [showSelectedOption, setShowSelectedOption] = useState({})
+    const [show, setShow] = useState(false)
     const options = [
         { value: 'chocolate', label: 'Chocolate' },
         { value: 'strawberry', label: 'Strawberry' },
@@ -11,13 +12,14 @@ const DropdownMenu = () => {
 
     const handleChange = (selectedOption) => {
         setShowSelectedOption(selectedOption)
+        setShow(true)
     }
 
     return (
         <div>
-            <h1>DropdownMenu</h1>
+            <h1>Dropdown Menu</h1>
             <Select options={options} onChange={handleChange} />
-            <h2>Ice Cream: {showSelectedOption.value.charAt(0).toUpperCase() + showSelectedOption.value.slice(1)}</h2>
+            {show && <h2>Ice Cream: {showSelectedOption.value.charAt(0).toUpperCase() + showSelectedOption.value.slice(1)}</h2>}
         </div>
     )
 }
