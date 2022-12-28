12353bf7b85a95230471e64130174ff8a548c03d
Tim La <tlamarketing111@gmail.com>
2022-12-28 07:33:25 -0500

    able to select different options & capitalize first letter

--- frontend/src/components/DropdownMenu.jsx
+++ frontend/src/components/DropdownMenu.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Select from 'react-select'
 
 const DropdownMenu = () => {
+    const [showSelectedOption, setShowSelectedOption] = useState({})
     const options = [
         { value: 'chocolate', label: 'Chocolate' },
         { value: 'strawberry', label: 'Strawberry' },
         { value: 'vanilla', label: 'Vanilla' }
     ]
 
+    const handleChange = (selectedOption) => {
+        setShowSelectedOption(selectedOption)
+    }
+
     return (
         <div>
             <h1>DropdownMenu</h1>
-            <Select options={options} />
+            <Select options={options} onChange={handleChange} />
+            <h2>Ice Cream: {showSelectedOption.value.charAt(0).toUpperCase() + showSelectedOption.value.slice(1)}</h2>
         </div>
     )
 }
