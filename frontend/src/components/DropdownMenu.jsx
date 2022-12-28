e169ccb4647184c904990746eea401aaecea3230
Tim La <tlamarketing111@gmail.com>
2022-12-28 07:28:00 -0500

    Select component able to select options

--- frontend/src/components/DropdownMenu.jsx
+++ frontend/src/components/DropdownMenu.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Select from 'react-select'
 
 const DropdownMenu = () => {
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+    ]
+
     return (
-        <div>DropdownMenu</div>
+        <div>
+            <h1>DropdownMenu</h1>
+            <Select options={options} />
+        </div>
     )
 }
 
